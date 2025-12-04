33d59b916e5b068335f06314694255757baadaa2
henryqseb <hequijiaso@uide.edu.ec>
2025-12-04 12:58:46 +0000

    Bandit0

---
+++ scrip.sh
@@ -0,0 +1,12 @@
+#Crear y listar archivos
+echo "Hola linux" > archivo.txt
+#Echo sale hacia la salida estandar 
+cat archivo.txt
+#cat > Muestra el contenido completo del archivo
+cp  archivo.txt copia.txt
+#cp > Crea una copia extaca al archivo original
+ls -l
+#ls > Mostar la lista de archivos y directorios en el directorio actual
+# -l > hace la lista sea detallada mostrando la informacion
+rm archivo.txt
+#rm > Sirve para eliminar archivos
