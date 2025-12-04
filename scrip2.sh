d36293e1f6ccf67b45a9211b534b7637a87fa402
henryqseb <hequijiaso@uide.edu.ec>
2025-12-04 13:20:07 +0000

    vuevle A

---
+++ scrip2.sh
@@ -0,0 +1,8 @@
+uname -a
+#uname > Imprime información del sistema
+lscpu
+#lscpu > Proporciona un resumen detallado de la arquitectura de la CPU.
+free -h
+#free > Muestra información sobre la Memoria RAM y la Memoria Swap del sistema.
+df -h
+#df > Informa del espacio en disco.
